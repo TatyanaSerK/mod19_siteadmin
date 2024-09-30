6629dbb65fa86aa228f39ae77c2744a220c537ec
TatyanaSerK <tsk.kalinina@gmail.com>
2024-09-30 10:22:40 +0300

    HW4 v2

--- myapp/task/models.py
+++ myapp/task/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class test(models.Model):
+class Test(models.Model):
     city = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     numb = models.IntegerField()
